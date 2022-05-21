f0157dfc1c970537d0b22c2163d1137ce47b1607
adriianasilva <drisan19@gmail.com>
2022-05-20 23:49:07 -0300

    insert main

---
+++ java-basic/src/classes/Main.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Criando um objeto Pessoa chamado Adriana
+		Pessoa Adriana = new Pessoa();
+		
+		//Criando um objeto Casa chamado FG
+		Casa FG = new Casa();
+		
+		//Criando um objeto Livro chamado "Roube como um Artista"
+		Livro RoubecomoumArtista = new Livro();
+
+	}
+
+}
