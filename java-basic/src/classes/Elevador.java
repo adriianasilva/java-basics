31004fc4ad4c5c9db7620e22a94583e958951a0a
adriianasilva <drisan19@gmail.com>
2022-05-26 21:11:08 -0300

    inicio elevador

--- java-basic/src/classes/Elevador.java
+++ java-basic/src/classes/Elevador.java
@@ -1,7 +1,34 @@
 package classes;
 
-public class Elevador {
+import java.util.Scanner;
 
+public class Elevador {
+	
+	private static int andarAtual;
+	private static int totalAndares;
+	private static int capElevador;
+	private static int pessoas;
+	
+	public static void Inicializa(capElevador,totalAndares){
+		
+	}
+	
+	public static void Entra() {
+		
+	}
+	
+	public static void Sai() {
+		
+	}
+
+	public static void Sobe() {
+			
+	}
+	
+	public static void Desce() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
