1d6232d85bed5a548a5754e2fe1d2288f22891c0
adriianasilva <drisan19@gmail.com>
2022-05-26 21:30:45 -0300

    elevador

--- java-basic/src/classes/Elevador.java
+++ java-basic/src/classes/Elevador.java
@@ -9,29 +9,83 @@ public class Elevador {
 	private static int capElevador;
 	private static int pessoas;
 	
-	public static void Inicializa(capElevador,totalAndares){
-		
+	public static void Inicializa(capElevador, totalAndares){
+		andarAtual = 0;
+		pessoas = 0;
 	}
 	
 	public static void Entra() {
-		
+		if(capElevador<pessoas) {
+			pessoas++;
+		}else {
+			return;
+		}
 	}
 	
 	public static void Sai() {
-		
+		if (pessoas > 0) {
+			pessoas--;
+		}
 	}
 
 	public static void Sobe() {
-			
+		if(andarAtual < totalAndares) {
+			andarAtual++;
+		}else {
+			return;
+		}
 	}
 	
 	public static void Desce() {
-		
+		if(andarAtual != 0) {
+			andarAtual--;
+		}else {
+			return;
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Insira a capacidade do Elevador");
+		int capElevador = leia.nextInt();
+		
+		System.out.println("Insira o total de andares do prédio");
+		int totalAndares = leia.nextInt();
+		
+		Inicializa();
+	}
 
+	public static int getAndarAtual() {
+		return andarAtual;
+	}
+
+	public static void setAndarAtual(int andarAtual) {
+		Elevador.andarAtual = andarAtual;
+	}
+
+	public static int getTotalAndares() {
+		return totalAndares;
+	}
+
+	public static void setTotalAndares(int totalAndares) {
+		Elevador.totalAndares = totalAndares;
+	}
+
+	public static int getCapElevador() {
+		return capElevador;
+	}
+
+	public static void setCapElevador(int capElevador) {
+		Elevador.capElevador = capElevador;
+	}
+
+	public static int getPessoas() {
+		return pessoas;
+	}
+
+	public static void setPessoas(int pessoas) {
+		Elevador.pessoas = pessoas;
 	}
 
 }
