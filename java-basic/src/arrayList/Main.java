5dea6ceac7fc873f5c83c25daf4964c891950625
adriianasilva <drisan19@gmail.com>
2022-05-31 23:45:38 -0300

    questoes array list

---
+++ java-basic/src/arrayList/Main.java
@@ -0,0 +1,73 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String[] args) {
+		//Escreva um programa que crie um ArrayList. Adicione alguma cores (strings) e as imprima na tela
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add("Vermelho");
+		lista.add("Rosa");
+		lista.add("Amarelo");
+		
+		for(int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+		
+		//Escreva um programa para adicionar um elemento na primeira posição da lista.
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("Elemento");
+		list.set(0, "Elemento");
+		
+		System.out.println(list.get(0));
+		
+		//Escreva um programa para remover o terceiro elemento da lista
+		
+		ArrayList<String> listaRemove = new ArrayList<String>();
+		listaRemove.add(0,"Primeiro");
+		listaRemove.add(1,"Segundo");
+		listaRemove.add(2,"Terceiro");
+		listaRemove.add(3,"Quarto");
+		
+		listaRemove.remove(2);
+		
+		for(int i = 0; i < listaRemove.size(); i++) {
+			System.out.println(listaRemove.get(i));
+		}
+		
+		/*Faça um programa que receba a temperatura média de cada mês do
+			ano e armazene-as em uma lista. Após isto, calcule a média anual das
+			temperaturas e mostre todas as temperaturas acima da média anual,
+			e em que mês elas ocorreram (mostrar o mês por extenso: 1 –
+			Janeiro, 2 – Fevereiro, . . .*/
+		
+		ArrayList<Double> temperaturaMedia = new ArrayList<Double>();
+		double mediaAnual = 0;
+		double temperaturas = 0;
+		
+		System.out.println("Insira a temperatura média de cada mês do ano");
+		
+		for(int i = 0; i < 12; i++) {
+			temperaturaMedia.set(i, new Scanner(System.in).nextDouble());
+		}
+		
+		for(int i = 0; i < temperaturaMedia.size(); i++) {
+			temperaturas = temperaturas + temperaturaMedia.get(i);
+		}
+	
+		mediaAnual = temperaturas/12;
+		
+		System.out.println("A média anual de temperaturas foi: " + mediaAnual +"°C");
+		
+		/*for(int i = 0; i < temperaturaMedia.size(); i++) {
+			if(temperaturaMedia.get(i) >= mediaAnual) {
+				
+			}
+		}*/
+		
+	}
+}
